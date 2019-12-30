cfea7851a41fe6efe33aba61f2003a3a80a10093
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 18:44:40 +0000

    Conversion

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -1,15 +1,26 @@
 function rot13(str) { // LBH QVQ VG!
   var splitString = str.split(""); 
   var optArr = [];
-  let rx = /[A-Za-z]/
   
   for (var i = 0; i < splitString.length; i++){
-      
+      if(rx.test(splitString[i])){
+        optArr[i] = conversion(splitString[i]);
+      }
+      else{
+        optArr[i] = splitString[i];
+      }
   }
 
   var output;
   return output;
 }
+
+function conversion(str){
+
+}
+
+let rx = /[A-Za-z]/
+console.log();
 
 // Change the inputs below to test
 rot13("LBH QVQ VG");

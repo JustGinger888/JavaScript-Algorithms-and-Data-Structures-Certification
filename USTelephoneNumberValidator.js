cebf13a284e2481793d917f61bc04596f2a5e202
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 19:23:37 +0000

    Initial

--- USTelephoneNumberValidator.js
+++ USTelephoneNumberValidator.js
@@ -1,1 +1,11 @@
+function telephoneCheck(str) {
+  let rx = /^([1]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/;
+  if(rx.test(str)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
+telephoneCheck("555-555-5555");

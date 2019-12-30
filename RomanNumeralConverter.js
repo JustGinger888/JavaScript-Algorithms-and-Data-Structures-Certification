a9d169a680c0edfe242efce24e61fda8c48847e3
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 04:33:54 +0000

    Table HCD

--- RomanNumeralConverter.js
+++ RomanNumeralConverter.js
@@ -2,7 +2,16 @@ function convertToRoman(num) {
  return num;
 }
 
+const eqvArr= [(1,"I"), (4,"IV"), 
+               (5,"V"), (9,"IX"), 
+               (10,"X"), (40,"XL"), 
+               (50,"L"), (90,"XC" ), 
+               (100,"C"), (400,"CD"), 
+               (500,"D"), (900,"CM"), 
+               (1000,"M")];
+
 convertToRoman(36);
+console.log(eqvArr[1,1]);
 
 /*------------------------------------Decimal Table--------------------------------------
 Decimal value (v)	 | Roman numeral (n)

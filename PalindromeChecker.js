7e55671c725d13b4f82219c1150d48e78609b95d
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 03:38:46 +0000

    Reverse String

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -1,8 +1,27 @@
 function palindrome(str) {
-  str.replace(/\W/gi, '').toUpperCase();
+  //Initialise Variables
+  var initialString = str.replace(/\W/gi, '').toUpperCase();
+  var comparisonString = reverseString(initialString);
+  
+  //Comparison Function to be Added
+  if(){
+
+  }
   return true;
 }
 
+     
+function reverseString(str) {
+    //--------------------------------Reverse String----------------------------------------
+      // split() returns a new array
+      var splitString = str.split(""); 
+      // reverse() reverses the new array
+      var reverseArray = splitString.reverse();
+      // join() joins all elements of array into a string
+      var joinArray = reverseArray.join("");
+    //--------------------------------------------------------------------------------------
+    return joinArray;
+}
 
 
 palindrome("eye");

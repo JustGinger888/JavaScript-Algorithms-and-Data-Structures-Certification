daf09f9dc600074e9a239ddce13db6f7e5d87881
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 04:16:49 +0000

    Game Plan

--- RomanNumeralConverter.js
+++ RomanNumeralConverter.js
@@ -1,1 +1,30 @@
+function convertToRoman(num) {
+ return num;
+}
 
+convertToRoman(36);
+
+/*------------------------------------Decimal Table--------------------------------------
+Decimal value (v)	Roman numeral (n)
+1	                I
+4	                IV
+5	                V
+9	                IX
+10	                X
+40	                XL
+50	                L
+90	                XC
+100	                C
+400	                CD
+500	                D
+900	                CM
+1000	            M
+
+let x = value
+let v = Highest dec eqv
+
+x = x - v UNTIL x = 0
+
+OUTUT n
+--------------------------------------Decimal Table--------------------------------------
+*/

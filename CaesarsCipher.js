fe9cf668241710846322676fb3b487f5479c8511
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 18:35:56 +0000

    Create CaesarsCipher.js

---
+++ CaesarsCipher.js
@@ -0,0 +1,15 @@
+function rot13(str) { // LBH QVQ VG!
+  var splitString = str.split(""); 
+  var optArr = [];
+  let rx = /[A-Za-z]/
+  
+  for (var i = 0; i < splitString.length; i++){
+      
+  }
+
+  var output;
+  return output;
+}
+
+// Change the inputs below to test
+rot13("LBH QVQ VG");

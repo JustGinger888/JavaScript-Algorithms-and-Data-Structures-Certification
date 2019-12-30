3eeff4381355649cd38ea7bb2828592bd223cd10
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 19:24:39 +0000

    FNL

--- USTelephoneNumberValidator.js
+++ USTelephoneNumberValidator.js
@@ -1,11 +1,6 @@
 function telephoneCheck(str) {
-  let rx = /^([1]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/;
-  if(rx.test(str)){
-    return true;
-  }
-  else{
-    return false;
-  }
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
 }
 
 telephoneCheck("555-555-5555");

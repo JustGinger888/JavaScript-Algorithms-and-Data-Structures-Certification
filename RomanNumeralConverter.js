aa78a003600c720c2a5e14f3763bc8313c006120
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 04:19:13 +0000

    Update RomanNumeralConverter.js

--- RomanNumeralConverter.js
+++ RomanNumeralConverter.js
@@ -5,20 +5,20 @@ function convertToRoman(num) {
 convertToRoman(36);
 
 /*------------------------------------Decimal Table--------------------------------------
-Decimal value (v)	Roman numeral (n)
-1	                I
-4	                IV
-5	                V
-9	                IX
-10	                X
-40	                XL
-50	                L
-90	                XC
-100	                C
-400	                CD
-500	                D
-900	                CM
-1000	            M
+Decimal value (v)	 | Roman numeral (n)
+1	                 | I
+4	                 | IV
+5	                 | V
+9	                 | IX
+10	                | X
+40	                | XL
+50	                | L
+90	                | XC
+100	               | C
+400	               | CD
+500	               | D
+900	               | CM
+1000	              | M
 
 let x = value
 let v = Highest dec eqv

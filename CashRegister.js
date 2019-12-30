6068c9637ed1628ae3cb150d7a0117d3bd1a0141
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 20:11:04 +0000

    Start

--- CashRegister.js
+++ CashRegister.js
@@ -1,7 +1,36 @@
+var denom = [
+  { n: "ONE HUNDRED", v: 100.0 },
+  { n: "TWENTY", v: 20.0 },
+  { n: "TEN", v: 10.0 },
+  { n: "FIVE", v: 5.0 },
+  { n: "ONE", v: 1.0 },
+  { n: "QUARTER", v: 0.25 },
+  { n: "DIME", v: 0.1 },
+  { n: "NICKEL", v: 0.05 },
+  { n: "PENNY", v: 0.01 }];
+
 function checkCashRegister(price, cash, cid) {
-  var change;
+  var change = cash - price;
+  var status;
+  var output = {status: null, change: []};
+
+  var register = cid.reduce(
+    function(acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    }, 
+    { total: 0 }
+    );
+
   // Here is your change, ma'am.
-  return change;
+  return output;
+}
+
+function statusOpt(){
+  return "INSUFFICIENT_FUNDS"
+  return "CLOSED";
+  return "OPEN";
 }
 
 // Example cash-in-drawer array:

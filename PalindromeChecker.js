f5176263b5d42a51d21b3749d0fad9a8306f3788
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-30 03:30:23 +0000

    Testing Start

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -1,1 +1,11 @@
+function palindrome(str) {
+  str.replace(/\W/gi, '');
+  return true;
+}
 
+
+
+palindrome("eye");
+let p = "eye12";
+let rx = /\W/g;
+console.log(p.replace(/\W/gi, '').toUpperCase());

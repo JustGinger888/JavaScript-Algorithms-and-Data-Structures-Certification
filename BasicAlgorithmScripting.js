8afc02465dcf7d6fb0ab5e1def4cd3eae85a9f26
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-31 02:12:27 +0000

    Where do I Belong

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -127,8 +127,18 @@ function bouncer(arr) {
 }
 
 
-//------------------------------------
+//------------------------------------Where do I Belong
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
 
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num) return a;
+  }
+
+  return arr.length;
+}
 
 
 //------------------------------------

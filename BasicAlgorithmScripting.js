c91e18c6d23035b7843da303167966ffc8fdd6e4
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-31 01:54:56 +0000

    Truncate a String

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -79,8 +79,15 @@ function repeatStringNumTimes(str, num) {
 }
 
 
-//------------------------------------
-
+//------------------------------------Truncate a String
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
 
 
 //------------------------------------

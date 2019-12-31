75581dedcbab9cde380c491c459d8c383b906ce3
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-31 00:40:37 +0000

    Return Largest Numbers in Arrays

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -35,8 +35,24 @@ function findLongestWordLength(str) {
   return mxl;
 }
 
-//------------------------------------
+//------------------------------------Return Largest Numbers in Arrays
+function largestOfFour(arr) {
+  var results = [];
+  
+  for (var i = 0; i < arr.length; i++) {
+    var largestNumber = arr[i][0];
+    
+    for (var j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
 
+    results[i] = largestNumber;
+  }
+
+  return results;
+}
 
 //------------------------------------
 

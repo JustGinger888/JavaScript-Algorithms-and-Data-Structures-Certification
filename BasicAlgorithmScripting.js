537f463165799285ec4be3dfa9e899fd92e975c0
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-31 00:37:24 +0000

    Find the Longest Word in a String

--- BasicAlgorithmScripting.js
+++ BasicAlgorithmScripting.js
@@ -1,4 +1,4 @@
-//Convert Celsius to Fahrenheit
+//------------------------------------Convert Celsius to Fahrenheit
 function convertToF(celsius) {
   var fahrenheit = celsius * (9 / 5) + 32;
   if (typeof fahrenheit !== "undefined") {
@@ -8,7 +8,61 @@ function convertToF(celsius) {
   }
 }
 
-//Reverse a String
+//------------------------------------Reverse a String
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
+
+//------------------------------------Factorialize a Number
+function factorialize(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+}
+
+//------------------------------------Find the Longest Word in a String
+function findLongestWordLength(str) {
+  var words = str.split(' ');
+  var mxl = 0;
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > mxl) {
+      mxl = words[i].length;
+    }
+  }
+  
+  return mxl;
+}
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------
+
+
+//------------------------------------

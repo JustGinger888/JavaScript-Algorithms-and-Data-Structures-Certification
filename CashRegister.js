ef8325fcedd55428cb0b6bd4301f24b3bf4c89c7
Ethan Moffatt <ethanmoffatt888@icloud.com>
2019-12-31 00:02:00 +0000

    FNL

--- CashRegister.js
+++ CashRegister.js
@@ -1,48 +1,79 @@
-var denom = [
-  { n: "ONE HUNDRED", v: 100.0 },
-  { n: "TWENTY", v: 20.0 },
-  { n: "TEN", v: 10.0 },
-  { n: "FIVE", v: 5.0 },
-  { n: "ONE", v: 1.0 },
-  { n: "QUARTER", v: 0.25 },
-  { n: "DIME", v: 0.1 },
-  { n: "NICKEL", v: 0.05 },
-  { n: "PENNY", v: 0.01 }];
+var change;
+
+var currencyEQV = [
+    { name: "ONE HUNDRED", val: 100.0 },
+    { name: "TWENTY", val: 20.0 },
+    { name: "TEN", val: 10.0 },
+    { name: "FIVE", val: 5.0 },
+    { name: "ONE", val: 1.0 },
+    { name: "QUARTER", val: 0.25 },
+    { name: "DIME", val: 0.1 },
+    { name: "NICKEL", val: 0.05 },
+    { name: "PENNY", val: 0.01 }
+];
 
 function checkCashRegister(price, cash, cid) {
-  var change = cash - price;
-  var status;
-  var output = {status: null, change: []};
+    var output = { registerStatus: null, change: [] };
+    change = cash - price;
+  
+    var register = cid.reduce(
+      function(a, c) {
+        a.total += c[1];
+        a[c[0]] = c[1];
+        return a;
+      }, { total: 0 }
+    );
+  
+    return calculateOutput(register, output, cid);
+}
 
-  var register = cid.reduce(
-    function(acc, curr) {
-      acc.total += curr[1];
-      acc[curr[0]] = curr[1];
-      return acc;
-    }, 
-    { total: 0 }
+function calculateOutput(register, output, cid){
+    if (register.total === change) {
+        output.registerStatus = "CLOSED";
+        output.change = cid;
+        return output;
+    }
+
+    if (register.total < change) {
+        output.registerStatus = "INSUFFICIENT_FUNDS";
+        return output;
+    }
+
+    var changeArray = createChangeArray(register);
+
+    if (changeArray.length < 1 || change > 0) {
+        output.registerStatus = "INSUFFICIENT_FUNDS";
+        return output;
+    }
+    
+    else{
+        output.registerStatus = "OPEN";
+        output.change = changeArray;
+        return output;
+    }
+}
+
+function createChangeArray(register){ 
+  var tmpArr = currencyEQV.reduce(
+        function(a, c) {
+            var value = 0;
+            while (register[c.name] > 0 && change >= c.val) {
+                change -= c.val;
+                register[c.name] -= c.val;
+                value += c.val;
+                change = Math.round(change * 100) / 100;
+            }
+            
+            if (value > 0) {
+                a.push([c.name, value]);
+            }
+            return a;
+        }, []
     );
 
-  // Here is your change, ma'am.
-  return output;
+  return tmpArr;
 }
 
-function statusOpt(){
-  return "INSUFFICIENT_FUNDS"
-  return "CLOSED";
-  return "OPEN";
-}
-
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], 
                              ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);

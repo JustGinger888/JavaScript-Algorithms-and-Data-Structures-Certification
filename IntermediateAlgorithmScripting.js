6e6f3847bd372823df148537c5e2056f2f7d6a43
Ethan Moffatt <ethanmoffatt888@icloud.com>
2020-01-03 00:43:51 +0000

    Map the Debris

--- IntermediateAlgorithmScripting.js
+++ IntermediateAlgorithmScripting.js
@@ -279,27 +279,19 @@ function addTogether(a, b) {
 
 
 
-//-----------------------------------------------------
+//-----------------------------------------------------Map the Debris
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
 
+  for (var prop in arr) {
+    var orbitalPer = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(arr[prop].avgAlt + earthRadius, 3) / GM)
+    );
+    delete arr[prop].avgAlt;
+    arr[prop].orbitalPeriod = orbitalPer;
+  }
 
-
-//-----------------------------------------------------
-
-
-
-//-----------------------------------------------------
-
-
-
-//-----------------------------------------------------
-
-
-
-//-----------------------------------------------------
-
-
-
-//-----------------------------------------------------
-
-
+  return arr;
+}
 

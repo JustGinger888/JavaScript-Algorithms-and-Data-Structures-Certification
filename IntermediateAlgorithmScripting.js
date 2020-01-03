22b00760dae32d3358587c414a9f170096cc62ac
Ethan Moffatt <ethanmoffatt888@icloud.com>
2020-01-03 00:40:36 +0000

    Update IntermediateAlgorithmScripting.js

--- IntermediateAlgorithmScripting.js
+++ IntermediateAlgorithmScripting.js
@@ -253,7 +253,7 @@ function binaryAgent(str) {
 }
 
 
-//-----------------------------------------------------
+//-----------------------------------------------------Truth Checking
 function truthCheck(collection, pre) {
   return collection.every(function (element) {
     return element[pre];
